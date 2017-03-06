5de53c1478e5a1173022fa03cb50bdc7fb4a733c
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-06 22:09:23 +0000

    Added player and board moves

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,13 @@ class Board
   end
 
   def place_new_player(player)
+    player.location = nil
     @grid[0] = [player]
+  end
+
+  def move_player(player, roll)
+    @grid[player.location].shift
+    @grid[(player.location + roll)] = [player]
+    player.location += roll
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 require 'board'
 
 describe Board do
-  let(:player) { double('player') }
+  let(:player) { double('player', location: 0, :location= => nil) }
+  let(:die) { double('die', roll: 3) }
+
   subject(:board) { described_class.new(player) }
 
   context 'Board' do
@@ -19,8 +21,17 @@ describe Board do
     end
 
     it 'A new player is placed on the board at grid position 1' do
+      allow(player).to receive(:location=).with(0)
       board.place_new_player(player)
       expect(board.grid[0].first).to eq player
+    end
+
+    it 'A player is moved 3 spaces when the dice is rolled' do
+      allow(player).to receive(:location=).with(0)
+      board.place_new_player(player)
+      board.move_player(player, die.roll)
+      expect(board.grid[0].first).not_to eq player
+      expect(board.grid[3].first).to eq player
     end
   end
 end
