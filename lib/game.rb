72ddad5ccd09b07f28eeb59e4477d0240388c69f
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-06 17:46:18 +0000

    Game send player to die

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,17 @@
 class Game
-  attr_reader :player, :board
-  def initialize(player, board)
+  attr_reader :player, :board, :die
+  def initialize(player, board, die)
     @player = player
     @board = board
+    @die = die
   end
 
   def start_game(player)
     @board.place_new_player(player)
   end
+
+  def roll_die(player)
+    die.roll(player)
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,31 +4,50 @@ require 'game'
 describe Game do
   let(:player) { double('player') }
   let(:board) { double('board') }
-  subject(:game) { described_class.new(player, board) }
+  let(:die) { double('die') }
 
-  context 'Create game' do
-    it 'A game can be started' do
-      expect(game).to be_an_instance_of Game
+  subject(:game) { described_class.new(player, board, die) }
+
+  context 'Creation of game' do
+    context 'Create game' do
+      it 'A game can be started' do
+        expect(game).to be_an_instance_of Game
+      end
+    end
+
+    context 'Player' do
+      it 'A game can have a player' do
+        expect(game.player).to eq player
+      end
+    end
+
+    context 'Board' do
+      it 'A game can have a board' do
+        expect(game.board).to eq board
+      end
+    end
+
+    context 'Die' do
+      it 'A game can have a die' do
+        expect(game.die).to eq die
+      end
     end
   end
 
-  context 'Player' do
-    it 'A game can have a player' do
-      expect(game.player).to eq player
+  context 'Game-Play' do
+    context 'New Game' do
+      it "Board is sent 'place_new_player' with player" do
+        allow(board).to receive(:place_new_player).with(player)
+        game.start_game(player)
+        expect(game.board).to have_received(:place_new_player).with(player)
+      end
     end
-  end
-
-  context 'Board' do
-    it 'A game can have a board' do
-      expect(game.board).to eq board
-    end
-  end
-
-  context 'New Game' do
-    it "Board is sent 'place_new_player' with player" do
-      allow(board).to receive(:place_new_player).with(player)
-      game.start_game(player)
-      expect(game.board).to have_received(:place_new_player).with(player)
+    context 'Die' do
+      it "Die is sent 'roll' with player" do
+        allow(die).to receive(:roll)
+        game.roll_die(player)
+        expect(game.die).to have_received(:roll).with(player)
+      end
     end
   end
 end
